80df6665dad183f0b6236bd8513fc2b13b74c813
hbxeagle <hbxeagle@gmail.com>
2018-02-23 10:41:36 +0800

    first commit

---
+++ AGoodObjectCStart/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AGoodObjectCStart
+//
+//  Created by didi on 23/02/2018.
+//  Copyright © 2018 hbx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
